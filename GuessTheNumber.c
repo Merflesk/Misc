9f5a99e6fd30f051f11cd07c46cf5b4afd43285f
Merflesk <joachim.mathisen@gmail.com>
2020-04-03 22:18:27 +0200

    First prototype

--- GuessTheNumber.c
+++ GuessTheNumber.c
@@ -7,15 +7,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <jlmlib.h>
 
 int main()
 {
     /* Declare variables */
     int nTheNumber = (rand() % 50);
 
-    // clear the screen
-    clearScreen();
 
     // Print the random number:
     printf("%d", nTheNumber);
